4890a5d0a83d8e4cdd660bb0e3b9fe6340d5339b
AguilarJ11 <8664@holbertonstudents.com>
2024-04-02 23:10:43 +0000

    imdone...

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *aux;
+
+	aux = tree;
+
+	while (tree)
+	{
+		if (tree->right)
+		tree = tree->right;
+		else if (!tree->right && tree->left)
+			tree = tree->left;
+		if (!tree->right && !tree->left)
+		{
+			aux = tree;
+			if (aux->parent)
+			tree = aux->parent;
+			free(aux);
+		}
+	}
+}
